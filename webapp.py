28375c7e483dfda88a38dcf214bce7f130290f77
JayanthKumar <106326846+Jayanth93912@users.noreply.github.com>
2023-02-15 20:52:17 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -18,7 +18,7 @@ def loading_model():
 
 cnn = loading_model()
 st.write("""
-#  detect weather the patient is suffering from Pneumonia or not 
+# To detect weather the patient is suffering from Pneumonia or not 
 """)
 
 
